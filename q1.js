84899c5c310cdc6f3d6978cf0e775315655e1eab
arpit220 <arpitpachauri220@gmail.com>
2022-08-01 21:27:55 +0530

    firstcommit

---
+++ q1.js
@@ -0,0 +1,12 @@
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("success");
+    }, 4000);
+  })
+    .then(function (value) {
+      return value;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
